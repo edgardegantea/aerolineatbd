8e1ad755cbd86376ab14d1e111953ba3a67e2b59
SalvadorOrdz <salorsa@outlook.es>
2022-09-01 15:53:24 -0500

    Actualizar

--- aero/models.py
+++ aero/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+class Ciudad (models.Model):
+    nombre = models.CharField(verbose_name='Nombre de la ciudad', max_length=80)
+    cp = models.IntegerField(verbose_name= 'codigo postal', max_length=10)
+    fechaCreacion = models.DateTimeField(auto_now_add=True)
+    fechaActualizacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nombre
