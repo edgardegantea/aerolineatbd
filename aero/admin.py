9de7d451633c9ecfdf289020435aac9783ffe711
SalvadorOrdz <salorsa@outlook.es>
2022-09-05 12:35:59 -0500

    Actualizar

--- aero/admin.py
+++ aero/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Ciudad
+from .models import Ciudad, Estado, Direccion
 # Register your models here.
 
 admin.site.register(Ciudad)
+admin.site.register(Estado)
+admin.site.
